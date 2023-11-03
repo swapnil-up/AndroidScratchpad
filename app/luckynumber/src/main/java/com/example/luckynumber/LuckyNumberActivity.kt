de3d33c64549cf934bacf98873cb230a298cdefb
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-11-03 19:00:26 +0545

    A random number is now generated. The button when pressed lets you send the data in any application you want such as gmail or messenger. there is a title and a text sent but for messenger only text sent. Need to look into set type too

--- app/luckynumber/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
+++ app/luckynumber/src/main/java/com/example/luckynumber/LuckyNumberActivity.kt
@@ -1,10 +1,12 @@
 package com.example.luckynumber
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class LuckyNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +15,31 @@ class LuckyNumberActivity : AppCompatActivity() {
 
         val LuckyNumberResult:TextView=findViewById(R.id.LuckyNumberResult)
         val shareButton:Button=findViewById(R.id.shareButton)
+
         val username=receiveUsername()
-        Toast.makeText(this, username,Toast.LENGTH_LONG).show()
+        val number=randomNumberGenerator()
+        LuckyNumberResult.text = "$number"
+        shareButton.setOnClickListener {
+            shareData(username, number)
+        }
     }
 
     fun receiveUsername(): String {
         var bundle: Bundle? = intent.extras
         return bundle?.getString("EXTRA_NAME").toString()
+    }
+
+    fun randomNumberGenerator():Int{
+        return Random.nextInt(1000)
+    }
+
+    fun shareData(username:String, number:Int){
+        Intent(Intent.ACTION_SEND).also {
+            it.type = "text/plain"
+            it.putExtra(Intent.EXTRA_SUBJECT,"$username is sharing luck")
+            it.putExtra(Intent.EXTRA_TEXT,"Luck shared is $number")
+            startActivity(it)
+        }
     }
 
 }
