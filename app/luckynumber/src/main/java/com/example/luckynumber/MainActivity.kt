af971f4d5f1a0b571f8d147d701bd87a546273d2
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-11-09 14:43:57 +0545

    Learnt about options menu and how to make one. Need to have action bar for it. it's the three dots on the top right.

--- app/luckynumber/src/main/java/com/example/luckynumber/MainActivity.kt
+++ app/luckynumber/src/main/java/com/example/luckynumber/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.luckynumber
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -32,12 +35,29 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
         LuckyButton.setOnClickListener(){
             Intent(this, LuckyNumberActivity::class.java).also {
                 it.putExtra("EXTRA_NAME",LuckyNumber.text.toString())
                 startActivity(it)
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.new_game -> {
+                // Handle the click on the menu item
+                // For example, you can show a toast message
+                Toast.makeText(this, "Starting new game", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            // Add more cases for other menu items if needed
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
