5e348caa9c1a48e68d45fcc820867a38590e8062
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-11-09 14:12:44 +0545

    Learnt about plurals in android as well how to reference color from activity with code.

--- app/luckynumber/src/main/java/com/example/luckynumber/MainActivity.kt
+++ app/luckynumber/src/main/java/com/example/luckynumber/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,23 @@ class MainActivity : AppCompatActivity() {
 
         val LuckyNumber:EditText=findViewById(R.id.LuckyNumber)
         val LuckyButton:Button=findViewById(R.id.LuckyButton)
+
+        val Button:Button=findViewById(R.id.button)
+        val text:TextView=findViewById(R.id.textView3)
+
+
+        val color:Int=ContextCompat.getColor(this,R.color.primary)
+
+        val numberOfApples = 1
+        val applesString = resources.getQuantityString(R.plurals.apples_quantity, numberOfApples, numberOfApples)
+
+
+        Button.setOnClickListener(){
+            text.text = applesString
+            text.setBackgroundColor(color)
+        }
+
+
 
         LuckyButton.setOnClickListener(){
             Intent(this, LuckyNumberActivity::class.java).also {
