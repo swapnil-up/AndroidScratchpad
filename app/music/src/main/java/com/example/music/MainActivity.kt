374482de8dc7af7f7e550a33f7bf888efbe9e006
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-11-21 18:44:17 +0545

    Seek bar now moves and the song timer works when song is played

--- app/music/src/main/java/com/example/music/MainActivity.kt
+++ app/music/src/main/java/com/example/music/MainActivity.kt
@@ -3,23 +3,31 @@ package com.example.music
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import java.sql.Time
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mediaPlayer:MediaPlayer
+    private lateinit var handler: Handler
+    private lateinit var timeLeft: TextView
+    private lateinit var updateSongTime: Runnable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var timeLeft:TextView=findViewById(R.id.timeLeft)
+        timeLeft=findViewById(R.id.timeLeft)
         var songTitle:TextView=findViewById(R.id.songTitle)
         var seekBar:SeekBar=findViewById(R.id.seekBar)
         var pauseButton:Button=findViewById(R.id.pauseButton)
         var pressed:Boolean=false
+        var finalTime:Double
+        var currentTime:Double
 
 
         mediaPlayer=MediaPlayer.create(
@@ -28,12 +36,38 @@ class MainActivity : AppCompatActivity() {
         )
 
         seekBar.isClickable=false
+        handler = Handler()
+
+        updateSongTime=object :Runnable{
+            override fun run() {
+                currentTime=mediaPlayer.currentPosition.toDouble()
+                timeLeft.text=""+ String.format(
+                    "%d mmin, %d sec", TimeUnit.MILLISECONDS.toMinutes(currentTime.toLong()),
+                    TimeUnit.MILLISECONDS.toSeconds(currentTime.toLong()-TimeUnit.MINUTES.toSeconds(
+                        TimeUnit.MILLISECONDS.toMinutes(currentTime.toLong())
+                    ))
+                )
+
+
+                seekBar.progress=currentTime.toInt()
+                handler.postDelayed(this,100)
+            }
+        }
 
         pauseButton.setOnClickListener(){
             if (!pressed) {
                 mediaPlayer.start()
+                finalTime=mediaPlayer.duration.toDouble()
+                currentTime=mediaPlayer.currentPosition.toDouble()
+
+                timeLeft.text=currentTime.toString()
+                seekBar.max=finalTime.toInt()
+                seekBar.progress=currentTime.toInt()
+
+
                 pressed=true
                 pauseButton.background=ContextCompat.getDrawable(this, R.drawable.play)
+                handler.postDelayed(updateSongTime,100)
             }
             else{
                 mediaPlayer.pause()
